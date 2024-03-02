60bf4ff5e5c5c047e778dd095d59a8b15f72c30f
Egor <egorizzee@gmail.com>
2024-03-02 11:53:31 +0300

    telegraph fix

--- bot/src/main/java/com/gorges/bot/handlers/commands/LinkCommandHandler.java
+++ bot/src/main/java/com/gorges/bot/handlers/commands/LinkCommandHandler.java
@@ -9,13 +9,6 @@ import com.gorges.bot.models.domain.Command;
 import com.gorges.bot.models.domain.TelegraphArticle;
 import com.gorges.bot.repositories.UserRepository;
 import com.gorges.bot.services.MailService;
-import com.gorges.bot.utils.Utils;
-import nl.siegmann.epublib.domain.Author;
-import nl.siegmann.epublib.domain.Book;
-import nl.siegmann.epublib.domain.Metadata;
-import nl.siegmann.epublib.domain.Resource;
-import nl.siegmann.epublib.epub.EpubWriter;
-import nl.siegmann.epublib.service.MediatypeService;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -23,14 +16,11 @@ import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -50,7 +40,7 @@ public class LinkCommandHandler extends AbstractBookSender implements CommandHan
 
             TelegraphArticle article = getTelegraphArticle (articleName);
             File book = createBook (article.title(), article.author(), article.text());
-            //sendBook (chatId, book);
+            sendBook (chatId, book);
 
             deleteMessage (absSender, sendingMessage);
             sendSentMessage (absSender, chatId);
@@ -78,39 +68,18 @@ public class LinkCommandHandler extends AbstractBookSender implements CommandHan
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             JsonObject result = new Gson().fromJson(response.body(), JsonObject.class)
                 .getAsJsonObject("result");
-
             List<JsonElement> elements = result.get("content").getAsJsonArray().asList();
+
             List<JsonArray> arrays = elements.stream()
                 .map(JsonElement::getAsJsonObject)
                 .map(o -> o.get("children").getAsJsonArray())
                 .toList();
 
+            // todo remake as stream
             StringBuilder sb = new StringBuilder();
-
             for (JsonArray arr : arrays) {
                 for (JsonElement el : arr.asList()) {
-                    if (el.isJsonObject()) {
-                        JsonObject obj = el.getAsJsonObject();
-                        String tag = obj.get("tag").getAsString();
-                        switch (tag) {
-                            case "br" -> sb.append("<br>");
-                            case "a" -> {
-                                // todo implement links?
-                            }
-                            default -> {
-                                if (obj.has("children")) {
-                                    String taggedText = obj.get("children").getAsString();
-                                    sb
-                                        .append("<").append(tag).append(">")
-                                        .append(taggedText)
-                                        .append("</").append(tag).append(">");
-                                }
-                            }
-                        }
-
-                    } else {
-                        sb.append(el.getAsString());
-                    }
+                    sb.append(getTelegraphNodeHtml(el));
                 }
 
                 sb.append("<br>");
@@ -125,7 +94,30 @@ public class LinkCommandHandler extends AbstractBookSender implements CommandHan
         }
     }
 
-    private String getTelegraphHtmlText (JsonElement jsonElement) {
+    private String getTelegraphNodeHtml(JsonElement el) {
+        if (el.isJsonObject()) {
+            JsonObject object = el.getAsJsonObject();
+
+            String tag = object.get("tag").getAsString();
+            boolean closeTag =
+                !tag.equals("br") &&
+                !tag.equals("hr") &&
+                !tag.equals("img");
+            if  (tag.equals("a")) {
+                // todo process links
+            }
+
+            if (object.has("children")) {
+                final String openingTag = "<" + tag + ">";
+                final String closingTag = closeTag ? ("</" + tag + ">") : "";
+                return openingTag + object.getAsJsonArray("children").asList()
+                    .stream().map(this::getTelegraphNodeHtml)
+                    .collect(Collectors.joining()) + closingTag;
+            }
+        } else {
+            return el.getAsString();
+        }
+
         return "";
     }
 
